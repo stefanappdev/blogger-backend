5300262cdeb0f771f1046f28199f948ca688e739
stefanappdev <stevenbrown2dn@gmail.com>
2023-07-24 20:48:03 -0500

    stable version of backend

--- Schemas/Blog.js
+++ Schemas/Blog.js
@@ -1,9 +1,10 @@
 const mongoose=require('mongoose');
 
 const blogSchema=new mongoose.Schema({
+    _id_User:{type:String,required:true},
     title:{type:String,required:true},
     body:{type:String,required:true},
-   snippet:{type:String},
+    snippet:{type:String},
 })
 
 
